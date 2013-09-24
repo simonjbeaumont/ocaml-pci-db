7d57c2e137bb50d2e6a10bece45a1b209841a45b
Si Beaumont <simon.beaumont@citrix.com>
2013-09-24 12:10:28 +0100

    Got Pci_db using the lex/yacc parser to bootstrap
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- lib/main.ml
+++ lib/main.ml
@@ -1,22 +1,4 @@
-let of_ic path =
-	let ic = open_in "/usr/share/hwdata/pci.ids" in
-	let lexbuf = Lexing.from_channel ic in
-	Pci_ids_parser.file Pci_ids_lexer.token lexbuf
-
-let main () =
-	let cin =
-	if Array.length Sys.argv > 1
-	then open_in Sys.argv.(1)
-	else stdin
-	in
-	(* try *)
-		let lexbuf = Lexing.from_channel cin in
-		(* while true do *)
-			(* print_endline (string_of_token (Pci_ids_lexer.token lexbuf)) *)
-		(* done *)
-		Pci_ids_parser.file Pci_ids_lexer.token lexbuf
-	(* with End_of_file -> { Pci_db_types.classes = Pci_db_types.IdMap.empty; Pci_db_types.vendors = Pci_db_types.IdMap.empty } *)
-
-let _ = Printexc.record_backtrace true
-
-let _ = Printexc.print main ()
+let _ =
+	let path = Sys.argv.(1) in
+	let pci_db = Pci_db.of_file path in
+	Pci_db.print pci_db
--- lib/pci_db_types.ml
+++ lib/pci_db_types.ml
@@ -6,6 +6,10 @@ module Id = struct
 		| VENDOR_ID of int64
 		| DEVICE_ID of int64
 		| SUBDEVICE_ID of int64 * int64
+	let to_string = function
+		| CLASS_ID x | SUBCLASS_ID x | PROGIF_ID x -> Printf.sprintf "%02Lx" x
+		| VENDOR_ID x | DEVICE_ID x -> Printf.sprintf "%04Lx" x
+		| SUBDEVICE_ID (x, y) -> Printf.sprintf "%04Lx %04Lx" x y
 	let compare = compare
 end
 
@@ -42,4 +46,3 @@ type t = {
 	classes: classs IdMap.t;
 	vendors: vendor IdMap.t;
 }
-
