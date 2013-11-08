01045f599ecdedefb99acce5afe7782c62ac1d64
Si Beaumont <simon.beaumont@citrix.com>
2013-11-08 12:22:26 +0000

    Use hand-crafted mock pci.ids file for unit tests
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- lib_test/test_pci_db.ml
+++ lib_test/test_pci_db.ml
@@ -1,7 +1,15 @@
 open OUnit
 
+let rec foldi n f acc =
+	if n = 0 then acc else foldi (pred n) f (f acc)
+
+let test_db =
+	let open Filename in
+	String.concat dir_sep
+		[ (foldi 4 dirname Sys.executable_name); "lib_test"; "mock-pci.ids" ]
+
 let with_test_db (f: Pci_db.t -> unit) =
-	let pci_db = Pci_db.of_file "/usr/share/hwdata/pci.ids" in
+	let pci_db = Pci_db.of_file test_db in
 	f pci_db
 
 let test_of_file _ =
