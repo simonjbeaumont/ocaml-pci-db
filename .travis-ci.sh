e9f7b3626ded3098910d6e6cd8e743f0bd5d86fe
Si Beaumont <simon.beaumont@citrix.com>
2013-11-12 11:36:43 +0000

    Be more verbose in travis log
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,7 @@
-# Edit this for your own project dependencies
-OPAM_DEPENDS="obuild unix ounit"
-     
+# OPAM packages needed to build tests.
+OPAM_PACKAGES="obuild ounit"
+
+
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
 3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
@@ -10,13 +11,21 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
-     
+
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
+export OPAMVERBOSE=1
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
 opam init 
-opam install ${OPAM_DEPENDS}
-eval `opam config env`
+opam install ${OPAM_PACKAGES}
+
+eval `opam config -env`
 make
 make test
